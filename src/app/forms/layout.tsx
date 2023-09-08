9f2b5736cdc188c7ab8def3e5472f00231f61cfe
ZeberMVP <rubeen722@gmail.com>
2023-09-08 20:28:45 +0200

    fix: made forms client side

--- src/app/forms/layout.tsx
+++ src/app/forms/layout.tsx
@@ -1,69 +1,71 @@
-import Image from 'next/image'
+"use client";
 
-import { SidebarNav } from '@/app/forms/components/SidebarNav'
-import { Separator } from '@/registry/new-york/ui/Separator'
+import Image from "next/image";
+
+import { SidebarNav } from "@/app/forms/components/SidebarNav";
+import { Separator } from "@/registry/new-york/ui/Separator";
 
 const sidebarNavItems = [
-	{
-		title: 'Profile',
-		href: '/forms',
-	},
-	{
-		title: 'Account',
-		href: '/forms/account',
-	},
-	{
-		title: 'Appearance',
-		href: '/forms/appearance',
-	},
-	{
-		title: 'Notifications',
-		href: '/forms/notifications',
-	},
-	{
-		title: 'Display',
-		href: '/forms/display',
-	},
-]
+  {
+    title: "Profile",
+    href: "/forms",
+  },
+  {
+    title: "Account",
+    href: "/forms/account",
+  },
+  {
+    title: "Appearance",
+    href: "/forms/appearance",
+  },
+  {
+    title: "Notifications",
+    href: "/forms/notifications",
+  },
+  {
+    title: "Display",
+    href: "/forms/display",
+  },
+];
 
 interface SettingsLayoutProps {
-	children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export default function SettingsLayout({ children }: SettingsLayoutProps) {
-	return (
-		<>
-			<div className='md:hidden'>
-				<Image
-					src='/forms-light.png'
-					width={1280}
-					height={791}
-					alt='Forms'
-					className='block dark:hidden'
-				/>
-				<Image
-					src='/forms-dark.png'
-					width={1280}
-					height={791}
-					alt='Forms'
-					className='hidden dark:block'
-				/>
-			</div>
-			<div className='hidden space-y-6 p-10 pb-16 md:block'>
-				<div className='space-y-0.5'>
-					<h2 className='text-2xl font-bold tracking-tight'>Settings</h2>
-					<p className='text-muted-foreground'>
-						Manage your account settings and set e-mail preferences.
-					</p>
-				</div>
-				<Separator className='my-6' />
-				<div className='flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0'>
-					<aside className='-mx-4 lg:w-1/5'>
-						<SidebarNav items={sidebarNavItems} />
-					</aside>
-					<div className='flex-1 lg:max-w-2xl'>{children}</div>
-				</div>
-			</div>
-		</>
-	)
+  return (
+    <>
+      <div className="md:hidden">
+        <Image
+          src="/forms-light.png"
+          width={1280}
+          height={791}
+          alt="Forms"
+          className="block dark:hidden"
+        />
+        <Image
+          src="/forms-dark.png"
+          width={1280}
+          height={791}
+          alt="Forms"
+          className="hidden dark:block"
+        />
+      </div>
+      <div className="hidden space-y-6 p-10 pb-16 md:block">
+        <div className="space-y-0.5">
+          <h2 className="text-2xl font-bold tracking-tight">Settings</h2>
+          <p className="text-muted-foreground">
+            Manage your account settings and set e-mail preferences.
+          </p>
+        </div>
+        <Separator className="my-6" />
+        <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
+          <aside className="-mx-4 lg:w-1/5">
+            <SidebarNav items={sidebarNavItems} />
+          </aside>
+          <div className="flex-1 lg:max-w-2xl">{children}</div>
+        </div>
+      </div>
+    </>
+  );
 }

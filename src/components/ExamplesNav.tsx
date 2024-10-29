c68c4ea14636b189ff4331c5f41cc1fe017ff342
Rubén Zafra <rubeen722@gmail.com>
2024-10-29 16:48:01 +0100

    added mail example

--- src/components/ExamplesNav.tsx
+++ src/components/ExamplesNav.tsx
@@ -1,77 +1,108 @@
-'use client'
+"use client";
 
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { ArrowRightIcon } from "@radix-ui/react-icons";
 
-import { cn } from '@/lib/utils'
-import { ScrollArea, ScrollBar } from '@/registry/new-york/ui/ScrollArea'
-import ThemeSelector from './ThemeSelector'
+import { cn } from "@/lib/utils";
+import { ScrollArea, ScrollBar } from "@/registry/new-york/ui/ScrollArea";
 
 const examples = [
-	{
-		name: 'Dashboard',
-		href: '/',
-		code: 'https://github.com/shadcn/ui/tree/main/apps/www/app/examples/dashboard',
-	},
-	{
-		name: 'Cards',
-		href: '/cards',
-		code: 'https://github.com/shadcn/ui/tree/main/apps/www/app/examples/cards',
-	},
-	{
-		name: 'Tasks',
-		href: '/tasks',
-		code: 'https://github.com/shadcn/ui/tree/main/apps/www/app/examples/tasks',
-	},
-	{
-		name: 'Playground',
-		href: '/playground',
-		code: 'https://github.com/shadcn/ui/tree/main/apps/www/app/examples/playground',
-	},
-	{
-		name: 'Forms',
-		href: '/forms',
-		code: 'https://github.com/shadcn/ui/tree/main/apps/www/app/examples/forms',
-	},
-	{
-		name: 'Music',
-		href: '/music',
-		code: 'https://github.com/shadcn/ui/tree/main/apps/www/app/examples/music',
-	},
-	{
-		name: 'Authentication',
-		href: '/authentication',
-		code: 'https://github.com/shadcn/ui/tree/main/apps/www/app/examples/authentication',
-	},
-]
+  {
+    name: "Mail",
+    href: "/",
+    code: "https://github.com/shadcn/ui/tree/main/apps/www/app/(app)/examples/mail",
+  },
+  {
+    name: "Dashboard",
+    href: "/dashboard",
+    code: "https://github.com/shadcn/ui/tree/main/apps/www/app/(app)/examples/dashboard",
+  },
+  {
+    name: "Cards",
+    href: "/cards",
+    code: "https://github.com/shadcn/ui/tree/main/apps/www/app/(app)/examples/cards",
+  },
+  {
+    name: "Tasks",
+    href: "/tasks",
+    code: "https://github.com/shadcn/ui/tree/main/apps/www/app/(app)/examples/tasks",
+  },
+  {
+    name: "Playground",
+    href: "/playground",
+    code: "https://github.com/shadcn/ui/tree/main/apps/www/app/(app)/examples/playground",
+  },
+  {
+    name: "Forms",
+    href: "/forms",
+    code: "https://github.com/shadcn/ui/tree/main/apps/www/app/(app)/examples/forms",
+  },
+  {
+    name: "Music",
+    href: "/music",
+    code: "https://github.com/shadcn/ui/tree/main/apps/www/app/(app)/examples/music",
+  },
+  {
+    name: "Authentication",
+    href: "/authentication",
+    code: "https://github.com/shadcn/ui/tree/main/apps/www/app/(app)/examples/authentication",
+  },
+];
 
 interface ExamplesNavProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export function ExamplesNav({ className, ...props }: ExamplesNavProps) {
-	const pathname = usePathname()
+  const pathname = usePathname();
 
-	return (
-		<div className='relative'>
-			<ScrollArea className='max-w-[600px] lg:max-w-none'>
-				<div className={cn('mb-4 flex items-center', className)} {...props}>
-					{examples.map((example) => (
-						<Link
-							href={example.href}
-							key={example.href}
-							className={cn(
-								'flex items-center px-4',
-								pathname?.endsWith(example.href)
-									? 'font-bold text-primary'
-									: 'font-medium text-muted-foreground'
-							)}
-						>
-							{example.name}
-						</Link>
-					))}
-				</div>
-				<ScrollBar orientation='horizontal' className='invisible' />
-			</ScrollArea>
-			<ThemeSelector />
-		</div>
-	)
+  return (
+    <div className="relative">
+      <ScrollArea className="max-w-[600px] lg:max-w-none">
+        <div className={cn("mb-4 flex items-center", className)} {...props}>
+          {examples.map((example, index) => (
+            <Link
+              href={example.href}
+              key={example.href}
+              className={cn(
+                "flex h-7 items-center justify-center rounded-full px-4 text-center text-sm transition-colors hover:text-primary",
+                pathname?.endsWith(example.href) ||
+                  (index === 0 && pathname === "/")
+                  ? "bg-muted font-medium text-primary"
+                  : "text-muted-foreground",
+              )}
+            >
+              {example.name}
+            </Link>
+          ))}
+        </div>
+        <ScrollBar orientation="horizontal" className="invisible" />
+      </ScrollArea>
+    </div>
+  );
+}
+
+interface ExampleCodeLinkProps {
+  pathname: string | null;
+}
+
+export function ExampleCodeLink({ pathname }: ExampleCodeLinkProps) {
+  const example = examples.find(
+    (example) => pathname?.startsWith(example.href),
+  );
+
+  if (!example?.code) {
+    return null;
+  }
+
+  return (
+    <Link
+      href={example?.code}
+      target="_blank"
+      rel="nofollow"
+      className="absolute right-0 top-0 hidden items-center rounded-[0.5rem] text-sm font-medium md:flex"
+    >
+      View code
+      <ArrowRightIcon className="ml-1 h-4 w-4" />
+    </Link>
+  );
 }

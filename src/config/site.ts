0991ee4b2686922842cc6fd5d1af42a11cb8c2e3
Rubén Zafra <rubeen722@gmail.com>
2024-11-04 21:57:30 +0100

    changed og image

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   name: "shadcn/ui themes",
   url: "https://shadcn-ui-themes.vercel.app/",
-  ogImage: "https://ui.shadcn.com/og.jpg",
+  ogImage: "/shadcn_ui-themes.jpg",
   description: "Try out themes for shadcn/ui components.",
   links: {
     twitter: "https://x.com/zebermvp",

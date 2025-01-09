b67d4a7ec66147accc6d530ebb42771098caaae0
Rubén Zafra <rubeen722@gmail.com>
2025-01-09 11:53:27 +0100

    added images for mail on mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,14 +15,14 @@ export default function MailPage() {
     <>
       <div className="md:hidden">
         <Image
-          src="/examples/mail-dark.png"
+          src="/mail-dark.png"
           width={1280}
           height={727}
           alt="Mail"
           className="hidden dark:block"
         />
         <Image
-          src="/examples/mail-light.png"
+          src="/mail-light.png"
           width={1280}
           height={727}
           alt="Mail"
